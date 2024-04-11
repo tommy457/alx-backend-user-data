82c1e57d4b379ebcc24bb413dd3296f0336a35cb
tommt457 <boukhalfaml1011@gmail.com>
2024-04-11 20:41:33 +0000

    fear: added encrypting passwords and check valid password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for hashing passwords and check if hash is valid.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ returns a salted, hashed password. """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate that the provided password matches the hashed password. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
