ffbc18a6b2dc1269c50ea4034333f96e86243283
tommy457 <boukhalfaml1011@gmail.com>
2024-04-18 21:24:20 +0100

    fix: bug fix in user_id_for_session_id

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -12,14 +12,15 @@ class SessionExpAuth(SessionAuth):
     def __init__(self) -> None:
         """ Constructor """
         try:
-            self.session_duration = int(os.getenv("SESSION_DURATION"))
+            duration = int(os.getenv("SESSION_DURATION"))
         except Exception:
-            self.session_duration = 0
+            duration = 0
+        self.session_duration = duration
 
     def create_session(self, user_id=None) -> str:
         """ creates a Session ID for a `user_id` """
         session_id = super().create_session(user_id)
-        if not session_id:
+        if session_id is None:
             return None
         session_dictionary = {}
         session_dictionary["user_id"] = user_id
@@ -31,13 +32,12 @@ class SessionExpAuth(SessionAuth):
     def user_id_for_session_id(self, session_id=None) -> str:
         """ returns a User ID based on a Session ID. """
         info = self.user_id_by_session_id.get(session_id)
-        user_id = self.info.get("user_id")
-        created_at = self.info.get("created_at")
+        if session_id is None or info is None:
+            return None
 
-        if session_id is None \
-                or info is None \
-                or user_id is None \
-                or created_at is None:
+        user_id = info.get("user_id")
+        created_at = info.get("created_at")
+        if user_id is None or created_at is None:
             return None
 
         if self.session_duration <= 0:
