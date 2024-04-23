94c1ea6143012fe22268f2a4cba3932d9e6fc74f
tommy457 <boukhalfaml1011@gmail.com>
2024-04-23 13:50:05 +0100

    fix: change import path

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,7 @@ Auth module
 import bcrypt
 from db import DB
 from user import User
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
